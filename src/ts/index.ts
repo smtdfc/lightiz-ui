1a7a46ed9acefa5e31731dc9e6f4d14d72649203
smtdfc <smtdfc.master@gmail.com>
2025-04-19 14:52:05 +0700

    update

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,3 +3,20 @@ export * from './events.js';
 export * from './module.js';
 export * from './toast.js';
 
+import * as components from './components/index.js';
+import * as events from './events.js';
+import * as module from './module.js';
+import * as toast from './toast.js';
+
+declare global {
+  interface Window {
+    LightizUI: Record<string, any>;
+  }
+}
+
+window.LightizUI = {
+  ...components,
+  ...events,
+  ...module,
+  ...toast,
+};
